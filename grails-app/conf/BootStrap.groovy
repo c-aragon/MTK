9613aa3a31640a34721d9d279ff00201bcecf835
Carlos Augusto Aragón Cruz <carlos.aragon@vincoorbis.com>
2017-06-15 02:10:40 -0500

    Add load data in Bootstrap file

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,8 @@
 import mx.mtk.Area
 import mx.mtk.Collaborator
+import mx.mtk.User
+import mx.mtk.Role
+import mx.mtk.UserRole
 import grails.util.Environment
 
 class BootStrap {
@@ -14,6 +17,21 @@ class BootStrap {
     		def area2 = new Area(name : "Operaciones Desarrollo de Software Usa y Canadá").save(flush : true, failOnError : true)
     		def area3 = new Area(name : "Administrativo").save(flush : true, failOnError : true)
     		def area4 = new Area(name : "Cultura Organizacional").save(flush : true, failOnError : true)
+
+            def userModificacion = new User(username : "modificacion", password : "12asd34", 
+                enabled : true, accountExpired : false, 
+                accountLocked : false, passwordExpired : false).save(flush : true, failOnError : true)
+
+            def userConsulta = new User(username : "consulta", password : "12asd34", 
+                enabled : true, accountExpired : false, 
+                accountLocked : false, passwordExpired : false).save(flush : true, failOnError : true)
+
+            def roleUser = new Role(authority : "ROLE_USER").save(flush : true, failOnError : true)
+            def roleAdmin = new Role(authority : "ROLE_ADMIN").save(flush : true, failOnError : true)
+
+            def temp01 = new UserRole(user : userModificacion, role: roleAdmin).save(flush : true, failOnError : true)
+            def temp02 = new UserRole(user : userConsulta, role: roleUser).save(flush : true, failOnError : true)
+
         }
     }
     def destroy = {
